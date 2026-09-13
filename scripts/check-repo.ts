kubohiroya/turbowarp-yaml-json

refactor(scripts): migrate repository scripts to TypeScript
Rename the scripts/*.mjs files to .ts and run them with `node scripts/*.ts`,
relying on Node's native type stripping rather than adding a tsx dependency.
The scripts gain explicit types instead of being inferred as `any`: an interface
mirroring the real repo-policy.json schema, the package.json fields the policy
check reads, the `npm pack --json` payload, and the block definitions consumed by
README generation.

engines.node moves to >=22.18.0, the first release that runs TypeScript sources
unflagged, and the policy check's expected engine literal moves with it.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

scripts/check-repo.ts
@@ -2,11 +2,61 @@ import {access, readFile} from 'node:fs/promises';
 import {execFile} from 'node:child_process';
 import {promisify} from 'node:util';
 
-const execFileAsync = promisify(execFile);
-const errors = [];
+interface PackageMetadata {
+  name: string;
+  version: string;
+  description?: string;
+  author?: string;
+  license?: string;
+  homepage?: string;
+  packageManager?: string;
+  engines?: {node?: string};
+  repository?: {url?: string};
+  bugs?: {url?: string};
+  files?: string[];
+  bin?: string | Record<string, string>;
+  main?: string;
+  types?: string;
+  scripts?: Record<string, string>;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  peerDependencies?: Record<string, string>;
+}
 
-const packageMetadata = JSON.parse(await readFile('package.json', 'utf8'));
-const policy = JSON.parse(await readFile('repo-policy.json', 'utf8'));
+interface RepoPolicy {
+  schemaVersion: number;
+  productName: string;
+  packageType: string;
+  licensePolicy: string;
+  readmeLanguages: string[];
+  canonicalReadme: string;
+  localizedReadmes: {
+    ja: string;
+  };
+  node: {
+    minimum: string;
+  };
+  packageManager: string;
+  requiredFiles: string[];
+  exceptions: {
+    upstreamFork: boolean;
+    mixedContentLicenses: boolean;
+    legacyPackageName: boolean;
+    thirdPartyBundle: boolean;
+  };
+  migrationChecklist: string[];
+}
+
+interface PackResult {
+  version: string;
+  files: {path: string}[];
+}
+
+const execFileAsync = promisify(execFile);
+const errors: string[] = [];
+
+const packageMetadata = JSON.parse(await readFile('package.json', 'utf8')) as PackageMetadata;
+const policy = JSON.parse(await readFile('repo-policy.json', 'utf8')) as RepoPolicy;
 const readme = await readFile(policy.canonicalReadme, 'utf8');
 const readmeJa = await readFile(policy.localizedReadmes.ja, 'utf8');
 const license = await readFile('LICENSE', 'utf8');
@@ -42,9 +92,10 @@ function checkPolicy() {
 }
 
 function checkPackageMetadata() {
-  const requiredStrings = ['description', 'author', 'license', 'homepage', 'packageManager'];
+  const requiredStrings = ['description', 'author', 'license', 'homepage', 'packageManager'] as const;
   for (const key of requiredStrings) {
-    if (typeof packageMetadata[key] !== 'string' || packageMetadata[key].trim().length === 0) {
+    const value = packageMetadata[key];
+    if (typeof value !== 'string' || value.trim().length === 0) {
       errors.push(`package.json ${key} must be a non-empty string`);
     }
   }
@@ -52,8 +103,8 @@ function checkPackageMetadata() {
   if (!packageMetadata.packageManager?.startsWith('pnpm@')) {
     errors.push('package.json packageManager must pin pnpm exactly');
   }
-  if (packageMetadata.engines?.node !== '>=22') {
-    errors.push('package.json engines.node must be >=22');
+  if (packageMetadata.engines?.node !== '>=22.18.0') {
+    errors.push('package.json engines.node must be >=22.18.0');
   }
   if (packageMetadata.repository?.url !== 'git+https://github.com/kubohiroya/turbowarp-yaml-json.git') {
     errors.push('package.json repository.url must point to the current repository');
@@ -122,7 +173,11 @@ function checkGeneratedArtifacts() {
 
 async function checkPackContents() {
   const {stdout} = await execFileAsync('npm', ['pack', '--dry-run', '--ignore-scripts', '--json']);
-  const [pack] = JSON.parse(stdout);
+  const [pack] = JSON.parse(stdout) as PackResult[];
+  if (!pack) {
+    errors.push('npm pack must report a package');
+    return;
+  }
   const files = new Set(pack.files.map((file) => file.path));
   for (const file of policy.requiredFiles) {
     if (!files.has(file)) errors.push(`npm pack must include ${file}`);
@@ -133,7 +188,7 @@ async function checkPackContents() {
   await access('pnpm-lock.yaml');
 }
 
-function extractConfigValue(key) {
+function extractConfigValue(key: string): string {
   const match = config.match(new RegExp(`${key}: '([^']+)'`));
   if (!match?.[1]) throw new Error(`src/config.ts must define ${key}`);
   return match[1];

kubohiroya/turbowarp-webrtc

scripts/check-repo.ts
@@ -3,11 +3,60 @@ import {readFile} from 'node:fs/promises';
 import process from 'node:process';
 import {promisify} from 'node:util';
 
-const execFileAsync = promisify(execFile);
-const errors = [];
+interface PackageMetadata {
+  name: string;
+  version: string;
+  description?: string;
+  author?: string;
+  license?: string;
+  homepage?: string;
+  packageManager?: string;
+  engines?: {node?: string};
+  repository?: {url?: string};
+  bugs?: {url?: string};
+  files?: string[];
+  publishConfig?: {access?: string; registry?: string};
+  bin?: string | Record<string, string>;
+  main?: string;
+  types?: string;
+  scripts?: Record<string, string>;
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  peerDependencies?: Record<string, string>;
+}
 
-const packageMetadata = JSON.parse(await readFile('package.json', 'utf8'));
-const policy = JSON.parse(await readFile('repo-policy.json', 'utf8'));
+interface RepoPolicy {
+  schemaVersion: number;
+  profile: string;
+  productName: string;
+  packageName: string;
+  extensionId: string;
+  bundle: string;
+  manifest: string;
+  canonicalReadme: string;
+  localizedReadmes: {
+    ja: string;
+  };
+  licensePolicy: string;
+  packageManager: string;
+  requiredFiles: string[];
+  runtimeBehaviorGuard: {
+    extensionId: string;
+    opcodes: string[];
+  };
+  relatedIssues: string[];
+}
+
+interface PackResult {
+  version: string;
+  files: {path: string}[];
+}
+
+const execFileAsync = promisify(execFile);
+const errors: string[] = [];
+
+const packageMetadata = JSON.parse(await readFile('package.json', 'utf8')) as PackageMetadata;
+const policy = JSON.parse(await readFile('repo-policy.json', 'utf8')) as RepoPolicy;
 const readme = await readFile(policy.canonicalReadme, 'utf8');
 const readmeJa = await readFile(policy.localizedReadmes.ja, 'utf8');
 const license = await readFile('LICENSE', 'utf8');
@@ -45,9 +94,10 @@ function checkPolicy() {
 }
 
 function checkPackageMetadata() {
-  const requiredStrings = ['description', 'author', 'license', 'homepage', 'packageManager'];
+  const requiredStrings = ['description', 'author', 'license', 'homepage', 'packageManager'] as const;
   for (const key of requiredStrings) {
-    if (typeof packageMetadata[key] !== 'string' || packageMetadata[key].trim().length === 0) {
+    const value = packageMetadata[key];
+    if (typeof value !== 'string' || value.trim().length === 0) {
       errors.push(`package.json ${key} must be a non-empty string`);
     }
   }
@@ -64,8 +114,8 @@ function checkPackageMetadata() {
   if (!/^pnpm@\d+\.\d+\.\d+$/u.test(packageMetadata.packageManager ?? '')) {
     errors.push('package.json packageManager must pin an exact pnpm version');
   }
-  if (packageMetadata.engines?.node !== '>=22') {
-    errors.push('package.json engines.node must be >=22');
+  if (packageMetadata.engines?.node !== '>=22.18.0') {
+    errors.push('package.json engines.node must be >=22.18.0');
   }
   if (packageMetadata.repository?.url !== 'git+https://github.com/kubohiroya/turbowarp-webrtc.git') {
     errors.push('package.json repository.url must point to the current repository');
@@ -155,7 +205,7 @@ function checkRuntimeGuard() {
   if (manifest.id !== policy.runtimeBehaviorGuard.extensionId) {
     errors.push('dist extension manifest ID must remain unchanged');
   }
-  const actualOpcodes = manifest.blocks.map((block) => block.opcode).sort();
+  const actualOpcodes = manifest.blocks.map((block: {opcode: string}) => block.opcode).sort();
   const expectedOpcodes = [...policy.runtimeBehaviorGuard.opcodes].sort();
   if (JSON.stringify(actualOpcodes) !== JSON.stringify(expectedOpcodes)) {
     errors.push('dist extension manifest opcodes must remain unchanged');
@@ -164,21 +214,27 @@ function checkRuntimeGuard() {
 
 function checkLegacyNames() {
   const legacy = new RegExp(['tm', 'pose'].join(''), 'iu');
-  for (const [fileName, text] of [
+  const legacyScanTargets: [fileName: string, text: string][] = [
     ['README.md', readme],
     ['README.ja.md', readmeJa],
     ['package.json', JSON.stringify(packageMetadata)],
     ['repo-policy.json', JSON.stringify(policy)],
-    [policy.bundle, bundle],
-    [policy.manifest, JSON.stringify(manifest)]
-  ]) {
+    [policy.bundle ?? '', bundle],
+    [policy.manifest ?? '', JSON.stringify(manifest)]
+  ];
+
+  for (const [fileName, text] of legacyScanTargets) {
     if (legacy.test(text)) errors.push(`${fileName} must not contain legacy pose-era naming`);
   }
 }
 
 async function checkPackContents() {
   const {stdout} = await execFileAsync('npm', ['pack', '--dry-run', '--ignore-scripts', '--json']);
-  const [pack] = JSON.parse(stdout);
+  const [pack] = JSON.parse(stdout) as PackResult[];
+  if (!pack) {
+    errors.push('npm pack must report a package');
+    return;
+  }
   const files = new Set(pack.files.map((file) => file.path));
   const expected = new Set(policy.requiredFiles);
   for (const file of expected) {
@@ -189,7 +245,7 @@ async function checkPackContents() {
   }
 }
 
-function extractConfigValue(key) {
+function extractConfigValue(key: string): string {
   const match = config.match(new RegExp(`${key}: '([^']+)'`, 'u'));
   if (!match?.[1]) throw new Error(`src/config.ts must define ${key}`);
   return match[1];